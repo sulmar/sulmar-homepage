d9d5730a9666d1a0fa7c550e99a1bcea934c7cb3
Jerzy <118739912+jszufa@users.noreply.github.com>
2024-03-25 15:15:57 +0100

    issue#37: global layout added

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -8,6 +8,7 @@ import * as directives from 'vuetify/directives';
 import 'vue3-carousel/dist/carousel.css'
 import { Carousel, Slide, Navigation} from 'vue3-carousel'
 
+import GlobalLayout from './layouts/GlobalLayout.vue'
 import Home from './layouts/Home.vue';
 import Course from './layouts/Course.vue';
 
@@ -26,6 +27,7 @@ export default defineClientConfig({
         app.component('Navigation', Navigation);
     },
     layouts: {
+        GlobalLayout,
         Home,
         Course,
     },
